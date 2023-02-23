c85f2cc88076d64e3662f04db8d6c321712da76a
charat <charatkosit@gmail.com>
2023-02-23 12:02:22 +0700

    env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  loginResult: "loginResult",
+  baseUrl: "",
+  loginUrl: "%loginUrl%",
+  backendUrl: "%backendUrl%",
+  sapUrl:"%sapUrl%",
+  sapToken:"%sapToken%",
+  project: "Build %stage%:%timestamp%",
+  release: "%release%"
 };
