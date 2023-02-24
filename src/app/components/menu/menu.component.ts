a4fb224a17b294fd2001773730b2a03580e74824
charat <charatkosit@gmail.com>
2023-02-24 19:29:46 +0700

    rev0.0.5

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
+  user:string = '';
   constructor() { }
 
   ngOnInit(): void {
+    this.user = environment.user;
   }
 
 }
